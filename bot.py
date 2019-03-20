5cb8e43386f8a42ac368475ff3c266fb7ba03567
Meed223 <henryrlewis@btinternet.com>
2019-03-20 22:12:41 +0000

    Changed around how file was written to be a little cleaner & tidy up import section.

--- bot.py
+++ bot.py
@@ -3,12 +3,22 @@ Welcome to StockImageBot
 A discord bot written in Python for StockImageSharks & N0ICE
 '''
 
+import discord
 from discord.ext.commands import Bot
 
+#---[ Bot Setup ]---
 TOKEN = ''
 BOT_PREFIX = ''
 
-client = Bot(command_prefix=BOT_PREFIX)
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # Bot should not reply to itself
+    if message.author == client.user:
+        return
+
+    # Check what command was and call appropriate function
 
 #---[ Bot Commands ]---
 
