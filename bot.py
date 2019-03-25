d639183877c58cef22528bb5c5a4f978a7ddf53b
Ben <br17011@essex.ac.uk>
2019-03-25 02:20:50 +0000

    work

--- bot.py
+++ bot.py
@@ -8,17 +8,19 @@ from discord.ext.commands import Bot
 
 #---[ Bot Setup ]---
 TOKEN = "Mzg5MTMxODA0NjI5NTMyNjcz.D3nFgQ.mYtDdynguWacx2k81xmh9wj27Ww"
-BOT_PREFIX = ''
+BOT_PREFIX = '}'
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
+    print(message.content)
     # Bot should not reply to itself
-    if message.author == client.user:
-        return
+    if message.author != client.user:
+        # Check what command was and call appropriate function
+        if message.content == BOT_PREFIX + "hello":
+            await message.channel.send("working")
 
-    # Check what command was and call appropriate function
 
 #---[ Bot Commands ]---
 
