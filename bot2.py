f1d0d1c80be3cc8af8a3b78c133e59a99083fef8
meed223 <henryrlewis@btinternet.com>
2019-12-16 14:37:52 +0000

    Added actual bot token, commented out testing token.

--- bot2.py
+++ bot2.py
@@ -21,9 +21,15 @@ from steam import SteamID
 client = discord.Client()
 
 # ---[ Bot Variables ]---
+# Actual bot token
+TOKEN = "Mzg5MTMxODA0NjI5NTMyNjcz.D3sVag.ucJKODmE1y8oG5lvhYIhgHIeWOs"
+BOT_PREFIX = "}"
+
+'''
 # Testing bot token
 TOKEN = "NTU5ODk4NjI0MDg4MjExNDU2.D3u5fw.gVs5shbmR6_OysVkDnplpM1w3mk"
 BOT_PREFIX = "{"
+'''
 
 # ---[ Program Logging ]---
 logger = logging.getLogger('StockImageBot.bot2')
